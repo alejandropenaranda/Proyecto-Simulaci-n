52782dc1c81212c9b06867ad74b7a806ec30a37f
alejandropenaranda <98985802+alejandropenaranda@users.noreply.github.com>
2023-10-23 17:20:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -48,15 +48,32 @@ by = [[0],
 x = np.zeros ((12,1)) # solucion inicial
 
 def Jacobi(A,b,x,n,tol): # n es numero de iteraciones
-  D = np.diag(A).reshape(x.shape)
-  R = A - np.diagflat(D)
-  for i in range(n):
-    print('iter', i)
-    xi = x
-    x = (b - np.dot(R,x))/D
-    tolx = np.linalg.norm(x-xi)/np.linalg.norm(x)
-    if tolx<tol:
-      break
-  return x
+    D = np.diag(A).reshape(x.shape)
+    R = A - np.diagflat(D)
+    for i in range(n):
+        xi = x
+        x = (b - np.dot(R,x))/D
+        tolx = np.linalg.norm(x-xi)/np.linalg.norm(x)
+        if tolx<tol:
+            break
+    print('numero de iteraciones',i)
+    print('tol J',tolx)    
+    return x
 
-print(Jacobi(A,bx,x,30,0.0000000001))
+print( 'resultado Jaco',Jacobi(A,bx,x,50,0.00000000001))
+
+def Jacobi_Sobre_Relajacion(A,b,x,n,tol,omega): # n es numero de iteraciones, omega debe ser mayor que 1 para sobreRelajación
+    D = np.diag(A).reshape(x.shape)
+    R = A - np.diagflat(D)
+    for i in range(n):
+        xi = x
+        x = (1-omega)*xi + omega*(b - np.dot(R,x))/D
+        tolx = np.linalg.norm(x-xi)/np.linalg.norm(x)
+        if tolx<tol:
+            break
+    print('numero de iteraciones',i)
+    print('tol JS',tolx)
+    return x
+
+print('resultado Jacos',Jacobi_Sobre_Relajacion(A,bx,x,50,0.00000000001,1.013))
+
